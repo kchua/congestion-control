19adfdf2c578eccc221b55a803a57eede0d5c990
Deniz Oktay <doktay@princeton.edu>
2020-12-04 12:45:02 -0800

    tcp tahoe and reno

--- client.py
+++ client.py
@@ -57,6 +57,8 @@ class TCPClient:
         server argument.
         """
 
+        self.algo = 'RENO'
+
         # Connection that will be initialized once.
         self.server = server  # Server identified with an (ip, port) pair.
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -75,12 +77,11 @@ class TCPClient:
         # Contains {timestamp, packet, retransmitted, ACKed}. Retransmits will happen upon timeout.
         # We can also use this to set estimated RTT, when retransmitted = False.
         self.packets_in_flight = []  # Priority by sequence number
-        self.retransmit_queue = []  # Priority by retransmission timer
 
-        self.estimated_rtt = 1.0  # in seconds
-        self.deviation = 1.0
-        self.time_since_transmit = 0.0
-        self.retransmission_timeout = self.estimated_rtt * 2
+        # Keeping track of RTT and RTO according to RFC6298
+        self.srtt = -1.0  # in seconds
+        self.rttvar = -1.0
+        self.rto = 1.0
 
         self.num_bytes_since_measuring = 0
         self.num_packets_since_measuring = 0
@@ -88,10 +89,12 @@ class TCPClient:
         self.measure_intervals = 1.0
 
         # Congestion control
-        self.congestion_window = 1
+        self.cwnd = 3  # Initial window set to 3 packets.
+        self.ssthresh = 1000  # Initially set to arbitrarily high.
+        self.rwnd = 200  # Manually set to not overwhelm receiver.
         self.slow_start = True
         self.duplicate_acks = 0
-        self.time_of_congestion_event = 0.0
+        self.time_of_transmit = 0.0
 
     def syn_packet(self):
         return create_packet(self.our_seq, 0, "", 0, IS_SYN)
@@ -100,11 +103,10 @@ class TCPClient:
         return create_packet(self.our_seq, self.ack_seq + 1, "", 0, IS_ACK)
 
     def compute_measurements(self):
-        logging.warn('Estimated RTT is: {}'.format(self.estimated_rtt))
+        logging.warn('Estimated RTT is: {}'.format(self.srtt))
         logging.warn('Number of packets in flight: {}'.format(len(self.packets_in_flight)))
-        logging.warn('Retransmission queue size: {}'.format(len(self.retransmit_queue)))
-        logging.warn('Retransmission timeout: {}'.format(self.retransmission_timeout))
-        logging.warn('Congestion Window: {}'.format(self.congestion_window))
+        logging.warn('Retransmission timeout: {}'.format(self.rto))
+        logging.warn('Congestion Window: {}'.format(self.cwnd))
         logging.warn('SLOW START: {}'.format(self.slow_start))
         self.last_time_measured = t.time()
         self.num_bytes_since_measuring = 0
@@ -158,7 +160,7 @@ class TCPClient:
                         decoded_msg = json.loads(msg.decode())
 
                         if addr != self.server:
-                            logging.warn('Received packet from identified server.')
+                            logging.warn('Received packet from unidentified server.')
                             continue
 
                         if (decoded_msg['flags'] & IS_SYN) and (decoded_msg['flags'] & IS_ACK):
@@ -170,7 +172,6 @@ class TCPClient:
 
                                 self.send_packet(self.ack_packet())
                                 self.state = ESTABLISHED
-                                self.time_of_congestion_event = t.time()
                                 logging.warning('Connection established for Client.')
 
 
@@ -198,7 +199,7 @@ class TCPClient:
                 # Check if we have any new ACKs
                 try:
                     msg, addr = self.sock.recvfrom(1600)
-                    received = t.time()
+                    received_time = t.time()
                     decoded_msg = json.loads(msg.decode())
 
                     if decoded_msg['flags'] & IS_ACK:
@@ -207,8 +208,11 @@ class TCPClient:
                             self.duplicate_acks += 1
 
                             if self.duplicate_acks == 3:
-                                # Fast retransmit
-                                pass
+                                if self.algo == 'RENO':
+                                    # Reno has fast recovery
+                                    self.retransmit_packet(True)
+                                else:
+                                    self.retransmit_packet(False)
                         else:
                             self.duplicate_acks = 0
 
@@ -220,27 +224,35 @@ class TCPClient:
                                 acked_infodict = heapq.heappop(self.packets_in_flight)[1]
 
                                 if self.slow_start:
-                                    self.congestion_window += 1.0
-                                    logging.warn('Congestion window updated to {}'.format(self.congestion_window))
-                                    logging.warn('Retransmit timeout {}'.format(self.retransmission_timeout))
+                                    self.cwnd += 1.0
+                                    logging.info('Congestion window updated to {}'.format(self.cwnd))
+                                    logging.info('Retransmit timeout {}'.format(self.rto))
                                 else:
-                                    self.congestion_window += 1.0 / self.congestion_window
+                                    self.cwnd += 1.0 / self.cwnd
+
+                                if self.cwnd > self.ssthresh:
+                                    self.slow_start = False
 
                                 if acked_infodict['retransmitted'] == 0:
-                                    # Use to update Estimated RTT.
+                                    # Since this packet is not a retransmitted packet, use it to update srtt.
 
                                     # Jacobson/Karels Algorithm
-                                    sample_rtt = received - acked_infodict['timestamp']
-                                    difference = sample_rtt - self.estimated_rtt
-                                    self.estimated_rtt = self.estimated_rtt + difference * 0.125
-                                    self.deviation = self.deviation + 0.125 * (abs(difference) - self.deviation)
-                                    self.retransmission_timeout = 2 * self.estimated_rtt + 4 * self.deviation
+                                    # Also Karn Algorithm - Do not calculate from retransmitted packets.
+                                    sample_rtt = received_time - acked_infodict['timestamp']
+                                    if self.srtt < 0:
+                                        # First time setting srtt
+                                        self.srtt   = sample_rtt
+                                        self.rttvar = sample_rtt / 2.0
+                                        self.rto    = self.srtt + max(0.010, 4 * self.rttvar)
+                                    else:
+                                        alpha = 0.125
+                                        beta  = 0.25
 
-                                    # Karn Algorithm
-                                    #self.estimated_rtt = self.estimated_rtt * ALPHA + sample_rtt * (1.0 - ALPHA)
-                                    #self.retransmission_timeout = self.estimated_rtt * 2
-                                    #logging.warn('Updated RTT {}'.format(self.estimated_rtt))
-                                del acked_infodict['packet']        # Free memory associated with packet, since already ACKed
+                                        self.rttvar = (1 - beta) * self.rttvar + beta * abs(self.srtt - sample_rtt)
+                                        self.srtt   = (1 - alpha) * self.srtt + alpha * sample_rtt
+                                        self.rto    = self.srtt + max(0.010, 4 * self.rttvar)
+                                    self.rto = max(self.rto, 1)  # Always round up RTO.
+                                    self.rto = min(self.rto, 60)  # Maximum value 60 seconds.
                                 acked_infodict['ACKed'] = True
                             else:
                                 break
@@ -249,7 +261,7 @@ class TCPClient:
                     pass
 
                 # Check if we should transmit any new packets.
-                if self.congestion_window > len(self.packets_in_flight):
+                if min(self.rwnd, self.cwnd) > len(self.packets_in_flight):
                     # TODO this is where congestion control goes.
                     data = self.read_data(MAX_DATA_SIZE)
                     if len(data) == 0:
@@ -262,45 +274,45 @@ class TCPClient:
 
                         self.our_seq += len(data)
                         self.send_packet(packet)
-                        self.time_since_transmit = t.time()
+                        self.time_of_transmit = t.time()
                         infodict = {
-                            'timestamp': self.time_since_transmit,
+                            'timestamp': self.time_of_transmit,
                             'packet': packet,
                             'retransmitted': 0,
                             'ACKed': False
                         }
                         heapq.heappush(self.packets_in_flight, (packet['seqnum'], infodict))
-                        heapq.heappush(self.retransmit_queue, (infodict['timestamp'] + self.retransmission_timeout, infodict))
 
                 # Check if we should retransmit any existing packets.
                 current_time = t.time()
-                while len(self.retransmit_queue) > 0:
-                    if current_time > self.retransmit_queue[0][0]:
-                        if self.retransmit_queue[0][1]['ACKed']:
-                            heapq.heappop(self.retransmit_queue)
-                            #logging.warn('popping from retransmit queue')
-                        else:
-                            to_retransmit_infodict = heapq.heappop(self.retransmit_queue)[1]
-                            self.send_packet(to_retransmit_infodict['packet'])
+                if current_time > self.rto + self.time_of_transmit:
+                    self.retransmit_packet(False)
 
-                            self.congestion_window = max(self.congestion_window / 2.0, 1)
-                            self.slow_start = False
-
-                            to_retransmit_infodict['retransmitted'] += 1
-
-                            # Exponential backoff retransmission time when there is a retransmission.
-                            retransmission_time = t.time() + self.retransmission_timeout
-                            self.retransmission_timeout *= 2
-                            self.retransmission_timeout = min(self.retransmission_timeout, 5)  # Don't suddenly blow up.
-
-                            heapq.heappush(self.retransmit_queue, (retransmission_time, to_retransmit_infodict))
-                            logging.warn('Retransmitting packet with seqnum {}'.format(to_retransmit_infodict['packet']['seqnum']))
-                    else:
-                        break
             else:
                 logging.error('Incorrect TCP State.')
                 self.state = CLOSED
                 return
+
+    def retransmit_packet(self, fast_recovery):
+        assert not self.packets_in_flight[0][1]['ACKed']
+        to_retransmit = self.packets_in_flight[0][1]
+
+        self.send_packet(to_retransmit['packet'])
+        logging.warn('Client RETRANSMITTING packet.')
+        self.time_of_transmit = current_time
+
+        if to_retransmit['retransmitted'] == 0:
+            # RFC5681 Equation (4)
+            self.ssthresh = max(len(self.packets_in_flight), 2)
+
+        # Restart from small congestion window, update ssthresh, begin slow start.
+        if fast_recovery:
+            self.slow_start = False
+            self.cwnd = self.ssthresh
+        else:
+            self.slow_start = True
+            self.cwnd = 1
+        to_retransmit['retransmitted'] += 1
 
 
 if __name__ == '__main__':
